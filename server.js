ea20c924274fdedbe3aaa53d7da4fb1c110d0cbe
Brandon Leventhal <bleventh@gmail.com>
2014-12-03 23:05:43 -0800

    add skeleton structure for hashing

--- server.js
+++ server.js
@@ -7,11 +7,30 @@ server.listen(config.port);
 
 console.log('Server is running.');
 
+var collectionServerHash = function(name) {
+   // determine which server to send to 
+}
+
+var getServerHash = function(id, collection) {
+   // determine which server to do the get on
+}
+
 server.on('connection', function(socket) {
    socket = new JsonSocket(socket);
    socket.on('message', function(message) {
       console.log('Holy shit, I got a message:' + JSON.stringify(message));
       //socket.sendEndMessage({response: 'Thanks for your message bro'});
+      var cmd = message.command;
+
+      if (cmd === "put") {
+
+      } else if (cmd === "find") {
+
+      } else if (cmd === "get") {
+
+      }
    });
 });
+
+
 
