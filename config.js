4099c03fe5d507040a476beae418afad1d02ab00
James E. Bilous <jbilous@cslvm31.csc.calpoly.edu>
2014-12-04 17:10:00 -0800

    Added storage methods

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.nodes = ['cslvm32'];
-config.port =  424242;
+config.port =  424243;
 config.inputPath = '/home/jbilous/NodeSQL/input';
 //config.inputPath = '/Users/brandon/Code/560/NodeSQL/input';
 module.exports = config;
