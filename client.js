7f6376fae0193432d22e55c2003d74b2b3032aae
James E. Bilous <jbilous@cslvm31.csc.calpoly.edu>
2014-12-05 15:40:51 -0800

    Working distribution

--- client.js
+++ client.js
@@ -23,7 +23,8 @@ var initSubscriptions = function(socket) {
       socket = new JsonSocket(socket);
    
       socket.on('message', function(message) {
-         console.log('Got a message from server: ' + JSON.stringify(message));
+         console.log(socket);
+         console.log('NodeSQL Responds: ' + JSON.stringify(message));
       });
 
       socket.on('close', function(something) {
@@ -33,31 +34,26 @@ var initSubscriptions = function(socket) {
 };
 
 // Add a hashCode function to String for use in overall hashing
-// Lifted from http://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript-jquery
 String.prototype.hashCode = function() {
    var hash = 0, i, chr, len;
-   if (this.length == 0) return hash;
-   for(i = 0, len < this.length; i < len; i++) {
+   if (this.length == 0) {
+      return hash;
+   }
+   for(i = 0, len = this.length; i < len; i++) {
       chr = this.charCodeAt(i);
-      hash = ((hash << 5) - hash) + chr;
-      hash |= 0;
+      hash += chr;
    }
    return hash;
 }
 
 // Utility function to hash a command
 var documentServerHash = function(query) {
-   console.log('hashing ' + JSON.stringify(query));
    var jsonObject = eval('(' + query.json + ')');
    var collectionName = query.collection;
-   console.log('collection name ' + collectionName);
    var collectionKey  = collectionKeys[collectionName];
-   console.log('collection key ' + collectionKey);
-   console.log('json object ' + JSON.stringify(jsonObject))
    var collectionKeyValue = jsonObject[collectionKey];
-   console.log('collection key value ' + collectionKeyValue);
-   var server = (collectionName.hashCode() + collectionKey.hashCode() + collectionKeyValue.hashCode()) % numberOfServers;
-   return [server, (server + 1) % numberOfServers];
+   var serverCode = (collectionName.hashCode() + collectionKey.hashCode() + collectionKeyValue.hashCode());
+   return [serverCode % numberOfServers, (serverCode + 1)  % numberOfServers];
 }
 
 
@@ -86,14 +82,13 @@ rl.on('line', function (cmd) {
    if(query.command == 'create') {
       var jsonObject = eval('(' + query.json + ')'); 
       collectionKeys[query.collection] = jsonObject['key'];
-      console.log('collectionKeys is now ' + JSON.stringify(collectionKeys));
       nodes.forEach(function(node) {
-         console.log('sending a create query');
          node.sendMessage(query);
       });
    } else {
       //Dispatch the command
-      nodes[documentServerHash(query)[0]].sendMessage(query);
+      var hashCode = documentServerHash(query)[0];
+      nodes[hashCode].sendMessage(query);
    }
 });
 
